1d64dbf26770f42695893b0d3aa38e3dda082320
Bahageel88 <bhageel1988@gmail.com>
2024-12-06 22:19:09 +0300

    Integration

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -35,7 +35,7 @@ const Chat = () => {
       setLoading(true);
 
       try {
-        const response = await axios.post("http://localhost:5000/generate", {
+        const response = await axios.post("https://voice-assistant-ai-3bis.onrender.com/generate", {
           input: data.text,
         });
 
