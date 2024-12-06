23e44532f645c870e262cdf80c5f8f32ba747a7a
Bahageel88 <bhageel1988@gmail.com>
2024-12-06 20:17:12 +0300

    predeployment commit

--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -3,7 +3,7 @@ import './Title.css'; // assuming the CSS is in a separate file called Title.css
 
 const Title = () => {
   return (
-    <h1 className='title'>Patient AI Assistant</h1>
+    <h1 className='title'>US Medical License Exam Assistant</h1>
   );
 };
 
